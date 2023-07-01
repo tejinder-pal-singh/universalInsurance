57907f912ba760f11fc9e958f71d3882c46b9ce2
George <gex7777@gmail.com>
2023-07-01 16:41:19 +0530

    extend classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,20 @@
 export const Button = ({
   children,
   onClick,
+  className,
 }: {
   onClick?: () => void;
   children: any;
+  className?: string;
 }) => {
   return (
     <button
-      className="  bg-secondary
-    text-white
+      className={` ${className && className} 
     text-[20px]
     py-[15px]
     px-[25px]
  
-    rounded-[5px]"
+    rounded-[5px]`}
       onClick={onClick}
     >
       {children}
