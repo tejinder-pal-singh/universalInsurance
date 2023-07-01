d2b436bd91c12cf6462d8b835c01c1b939a29b5d
George <gex7777@gmail.com>
2023-07-02 02:03:35 +0530

    fix styles

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,7 @@
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
 export const Contact = () => {
   return (
     <section className="bg-neutral relative  overflow-hidden">
@@ -8,7 +12,7 @@ export const Contact = () => {
       <div className="navbar-spacer invisible pt-10 lg:py-20">
         navbar plachoder
       </div>
-      <div className="relative z-20 container px-6 mx-auto">
+      <div className="relative px-10 lg:px-32 z-20  w-full">
         <div>
           <p className="text-3xl lg:text-6xl z-20 font-bold dark:text-secondary">
             Contact us
@@ -22,24 +26,11 @@ export const Contact = () => {
           </p>
         </div>
 
-        <div className="grid text-lg z-20 pb-10 grid-cols-1 gap-12 mt-10 lg:grid-cols-2">
+        <div className="grid text-lg z-20 w-full justify-between pb-10 grid-cols-1 gap-12 mt-10 lg:grid-cols-2">
           <div className="grid  grid-cols-1 gap-12 md:grid-cols-2">
             <div>
               <span className="inline-block p-3 text-primary rounded-full bg-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-                  />
-                </svg>
+                <EmailOutlinedIcon />
               </span>
 
               <h2 className="mt-4 text-xl font-medium text-primary ">Email</h2>
@@ -51,25 +42,7 @@ export const Contact = () => {
 
             <div>
               <span className="inline-block p-3 text-primary rounded-full bg-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                  />
-                </svg>
+                <ChatBubbleOutlineOutlinedIcon />
               </span>
 
               <h2 className="mt-4 text-xl font-medium text-primary ">
@@ -83,25 +56,7 @@ export const Contact = () => {
 
             <div>
               <span className="inline-block p-3 text-primary rounded-full bg-white ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                  />
-                </svg>
+                <LocationOnOutlinedIcon />
               </span>
 
               <h2 className="mt-4 text-xl font-medium text-primary">Office</h2>
@@ -115,20 +70,7 @@ export const Contact = () => {
 
             <div>
               <span className="inline-block p-3 text-primary rounded-full bg-white ">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                  />
-                </svg>
+                <PhoneOutlinedIcon />
               </span>
 
               <h2 className="mt-4 text-xl font-medium text-primary ">Phone</h2>
