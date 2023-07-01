0848c81684e526b041b965301a91364d7018717c
George <gex7777@gmail.com>
2023-07-01 15:53:42 +0530

    refactor styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
-import { SectionTwo } from '../components/InvestmentSection';
-import { SectionOne } from '../components/InsuranceSection';
-import { HeroSection } from '../components/HeroSection';
-import { Banner } from '../components/Banner';
+import { SectionTwo } from "../components/InvestmentSection";
+import { SectionOne } from "../components/InsuranceSection";
+import { HeroSection } from "../components/HeroSection";
+import { Banner } from "../components/Banner";
 
 const Home = () => {
-	return (
-		<>
-			<HeroSection />
-			<Banner />
-			<SectionOne />
-			<SectionTwo />
-		</>
-	);
+  return (
+    <>
+      <HeroSection />
+      <Banner />
+      <SectionOne />
+      <SectionTwo />
+    </>
+  );
 };
 
 export default Home;
