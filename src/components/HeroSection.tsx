c67b0d188c8ffc912121a0f7def3ec31f9178440
George <gex7777@gmail.com>
2023-07-02 06:08:29 +0530

    fix mobile ver

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Variants, motion } from "framer-motion";
-
+import familyCrop from "../assets/familyCrop.jpg";
 import "./styles.css";
 import { Button } from "./Button";
 const revealParent: Variants = {
@@ -14,13 +14,7 @@ const revealParent: Variants = {
     },
   },
 };
-const heroParent: Variants = {
-  initial: { opacity: 1 },
-  reveal: {
-    opacity: 1,
-    transition: { staggerChildren: 0.1 },
-  },
-};
+
 const textParent: Variants = {
   initial: { opacity: 1 },
   reveal: {
@@ -54,33 +48,77 @@ export const HeroSection = () => {
 
 function HeroContent() {
   return (
-    <motion.div
-      variants={revealParent}
-      initial="initial"
-      animate="reveal"
-      className="flex-container"
-    >
-      <motion.div
-        variants={textParent}
-        className="px-10 lg:pl-32 lg md:max-w-[560px] items-center backdrop-blur-sm py-4 my-auto hero-banner-headingText-container"
-      >
-        <motion.h1 variants={textReveal} className=" home-hero-heading ">
-          Is your family is <span className="oval">secured</span> from whatever
-          life has to offer?
-        </motion.h1>
-        <motion.p variants={textReveal} className="home-hero-desc py-4">
-          Well, surely we cannot predict the future but we can be ready for it.
-          Here at 'Universal Insurance', we ensure you to stand with you during
-          the time of urgency. For a better future of you and your family
-        </motion.p>
-        <motion.div variants={emailReveal}>
-          <Button className="text-white bg-secondary">
-            Schedule a call with our agent
-          </Button>
+    <>
+      {" "}
+      <div className="mobile  lg:hidden">
+        <div className="h-20"></div>
+        <motion.div
+          variants={revealParent}
+          className=" overflow-hidden"
+          initial="initial"
+          animate="reveal"
+        >
+          <img src={familyCrop} alt="family" className="contain" />
+          <motion.div
+            variants={textParent}
+            className="px-10 lg:pl-32  backdrop-blur-sm py-4 my-auto "
+          >
+            <motion.h1 variants={textReveal} className=" text-3xl break-words">
+              Is your family
+              <motion.span
+                variants={textReveal}
+                className="oval bg-secondary rounded-full  px-2 pb-2 text-white"
+              >
+                secured
+              </motion.span>
+              from whatever life has to offer?
+            </motion.h1>
+            <motion.p variants={textReveal} className="text-base pb-5">
+              Well, surely we cannot predict the future but we can be ready for
+              it. Here at 'Universal Insurance', we ensure you to stand with you
+              during the time of urgency. For a better future of you and your
+              family
+            </motion.p>
+            <motion.div variants={emailReveal}>
+              <Button className="text-white bg-secondary">
+                Schedule a call with our agent
+              </Button>
+            </motion.div>
+          </motion.div>
+          {/* <div className='hero-banner-img'></div>  */}
+        </motion.div>{" "}
+      </div>
+      <div className=" large-screens-only hidden lg:block">
+        <motion.div
+          variants={revealParent}
+          initial="initial"
+          animate="reveal"
+          className="flex-container"
+        >
+          <motion.div
+            variants={textParent}
+            className="px-10 lg:pl-32 lg md:max-w-[560px] items-center backdrop-blur-sm py-4 my-auto hero-banner-headingText-container"
+          >
+            <motion.h1 variants={textReveal} className=" home-hero-heading ">
+              Is your family is <span className="oval">secured</span> from
+              whatever life has to offer?
+            </motion.h1>
+            <motion.p variants={textReveal} className="home-hero-desc py-4">
+              Well, surely we cannot predict the future but we can be ready for
+              it. Here at 'Universal Insurance', we ensure you to stand with you
+              during the time of urgency. For a better future of you and your
+              family
+            </motion.p>
+            <motion.div variants={emailReveal}>
+              <Button className="text-white bg-secondary">
+                Schedule a call with our agent
+              </Button>
+            </motion.div>
+          </motion.div>
+          {/* <div className='hero-banner-img'></div>  */}
         </motion.div>
-      </motion.div>
-      {/* <div className='hero-banner-img'></div>  */}
-    </motion.div>
+      </div>
+    </>
   );
 }
 
