dc5bace8449f0ee420acae61887e5316a4c42a99
George <gex7777@gmail.com>
2023-07-19 03:11:29 +0530

    add supervisa route

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import Home from './pages/Home';
-import { NoMatch } from './pages/NoMatch';
-import { Contact } from './pages/Contact';
-import { Services } from './pages/Services';
-import { About } from './pages/About';
-import ScrollToTop from './components/ScrollToTop';
-import './components/styles.css';
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout";
+import Home from "./pages/Home";
+import { NoMatch } from "./pages/NoMatch";
+import { Contact } from "./pages/Contact";
+import { Services } from "./pages/Services";
+import { About } from "./pages/About";
+import ScrollToTop from "./components/ScrollToTop";
+import "./components/styles.css";
+import { Supervisa } from "./pages/Supervisa";
 function App() {
-	return (
-		<>
-			<ScrollToTop>
-				<Routes>
-					<Route path='/' element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path='about' element={<About />} />
-						<Route path='services' element={<Services />} />
-						<Route path='contact' element={<Contact />} />
+  return (
+    <>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="supervisa" element={<Supervisa />} />
 
-						{/* Using path="*"" means "match anything", so this route
+            {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-						<Route path='*' element={<NoMatch />} />
-					</Route>
-				</Routes>
-			</ScrollToTop>
-		</>
-	);
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
+    </>
+  );
 }
 
 export default App;
