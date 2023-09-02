a45e108d2bea849a644c5127875ea82da547227e
tejinder-pal-singh <dev.singhTejinder@gmail.com>
2023-09-01 23:54:33 -0400

    email service

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,182 +1,198 @@
-import { Button } from "./Button";
-import * as Yup from "yup";
-import { Formik, Field, Form as Formi, FormikHelpers } from "formik";
+import { Button } from './Button';
+import * as Yup from 'yup';
+import { Formik, Field, Form as Formi, FormikHelpers } from 'formik';
 
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  lastName: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  message: Yup.string(),
+	firstName: Yup.string()
+		.min(2, 'Too Short!')
+		.max(50, 'Too Long!')
+		.required('Required'),
+	lastName: Yup.string()
+		.min(2, 'Too Short!')
+		.max(50, 'Too Long!')
+		.required('Required'),
+	email: Yup.string().email('Invalid email').required('Required'),
+	message: Yup.string(),
 });
 
 export type Email = {
-  type:
-    | "general"
-    | "Life Insurance"
-    | "Super Visa Insurance"
-    | "Investments"
-    | "Travel Insurance";
-  message: string;
+	type:
+		| 'general'
+		| 'Life Insurance'
+		| 'Super Visa Insurance'
+		| 'Investments'
+		| 'Travel Insurance';
+	message: string;
 };
 const emailTemplates: Email[] = [
-  {
-    type: "general",
-    message: "Im interested in hearing more about the services offered!",
-  },
-  { type: "Life Insurance", message: "Need a quote regarding Life insurance" },
-  {
-    type: "Super Visa Insurance",
-    message: "Need a quote regarding Super Visa Insurance",
-  },
-  {
-    type: "Investments",
-    message: "Need a quote for investment opportunities offered",
-  },
-  {
-    type: "Travel Insurance",
-    message: "Need a quote for Travel Insurance",
-  },
+	{
+		type: 'general',
+		message: 'Im interested in hearing more about the services offered!',
+	},
+	{ type: 'Life Insurance', message: 'Need a quote regarding Life insurance' },
+	{
+		type: 'Super Visa Insurance',
+		message: 'Need a quote regarding Super Visa Insurance',
+	},
+	{
+		type: 'Investments',
+		message: 'Need a quote for investment opportunities offered',
+	},
+	{
+		type: 'Travel Insurance',
+		message: 'Need a quote for Travel Insurance',
+	},
 ];
 
 interface Values {
-  firstName: string;
-  lastName: string;
-  email: string;
-  message: string;
-  subject: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	message: string;
+	subject: number;
 }
 
-export const Form = ({ type }: { type: Email["type"] }) => {
-  console.log(type);
+export const Form = ({ type }: { type: Email['type'] }) => {
+	const sendMail = (userEmail, subject, msg) => {
+		const config = {
+			SecureToken: '68e84eee-0be4-4b74-82b9-ee323757b354',
+			// SecureToken: '0450a839-2b6c-4184-9964-46884e54ebe3',
+			To: 'mr.aashat@gmail.com',
+			From: 'universalinsured@gmail.com',
+			Subject: subject,
+			Body: msg,
+		};
+		if (window.Email) {
+			window.Email.send(config)
+				.then((msg) => {
+					console.log(msg);
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		}
+	};
 
-  return (
-    <div className="p-4 py-6 rounded-lg bg-white  md:p-8">
-      <Formik
-        enableReinitialize={true}
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          message:
-            type && !!emailTemplates.find((e) => e.type === type)?.message
-              ? emailTemplates.find((e) => e.type === type)?.message ?? ""
-              : emailTemplates[0].message,
-          subject:
-            type && emailTemplates.findIndex((e) => e.type === type)
-              ? emailTemplates.findIndex((e) => e.type === type)
-              : 0,
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-        }}
-      >
-        {(props) => (
-          <Formi>
-            <div className="-mx-2 md:items-center md:flex">
-              <div className="flex-1 px-2">
-                <label className="block mb-2 text-lg text-gray-600 dark:text-gray-200">
-                  First Name
-                </label>
-                {props.errors.firstName && props.touched.firstName ? (
-                  <div className="text-red">{props.errors.firstName}</div>
-                ) : null}
-                <Field
-                  autoFocus={true}
-                  name="firstName"
-                  type="text"
-                  placeholder="John "
-                  className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-              </div>
+	return (
+		<div className='p-4 py-6 rounded-lg bg-white  md:p-8'>
+			<Formik
+				enableReinitialize={true}
+				initialValues={{
+					firstName: '',
+					lastName: '',
+					email: '',
+					message:
+						type && !!emailTemplates.find((e) => e.type === type)?.message
+							? emailTemplates.find((e) => e.type === type)?.message ?? ''
+							: emailTemplates[0].message,
+					subject:
+						type && emailTemplates.findIndex((e) => e.type === type)
+							? emailTemplates.findIndex((e) => e.type === type)
+							: 0,
+				}}
+				validationSchema={SignupSchema}
+				onSubmit={(
+					values: Values,
+					{ setSubmitting }: FormikHelpers<Values>
+				) => {
+					sendMail(values.email, values.subject, values.message);
+					// setTimeout(() => {
+					// 	alert(JSON.stringify(values, null, 2));
+					// 	setSubmitting(false);
+					// }, 500);
+				}}>
+				{(props) => (
+					<Formi>
+						<div className='-mx-2 md:items-center md:flex'>
+							<div className='flex-1 px-2'>
+								<label className='block mb-2 text-lg text-gray-600 dark:text-gray-200'>
+									First Name
+								</label>
+								{props.errors.firstName && props.touched.firstName ? (
+									<div className='text-red'>{props.errors.firstName}</div>
+								) : null}
+								<Field
+									autoFocus={true}
+									name='firstName'
+									type='text'
+									placeholder='John '
+									className='block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+								/>
+							</div>
 
-              <div className="flex-1 px-2 mt-4 md:mt-0">
-                <label className="block mb-2 text-lg text-gray-600 dark:text-gray-200">
-                  Last Name
-                </label>
-                {props.errors.lastName && props.touched.lastName ? (
-                  <div className="text-red">{props.errors.lastName}</div>
-                ) : null}
-                <Field
-                  name="lastName"
-                  type="text"
-                  placeholder="Doe"
-                  className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-              </div>
-            </div>
+							<div className='flex-1 px-2 mt-4 md:mt-0'>
+								<label className='block mb-2 text-lg text-gray-600 dark:text-gray-200'>
+									Last Name
+								</label>
+								{props.errors.lastName && props.touched.lastName ? (
+									<div className='text-red'>{props.errors.lastName}</div>
+								) : null}
+								<Field
+									name='lastName'
+									type='text'
+									placeholder='Doe'
+									className='block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+								/>
+							</div>
+						</div>
 
-            <div className="mt-4">
-              <label className="block mb-2 text-lg text-gray-600 dark:text-gray-200">
-                Email address
-              </label>
-              {props.errors.email && props.touched.email ? (
-                <div className="text-red">{props.errors.email}</div>
-              ) : null}
-              <Field
-                name="email"
-                type="email"
-                placeholder="johndoe@example.com"
-                className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              />
-            </div>
-            <div className="mt-4">
-              <label className="block mb-2 text-lg text-gray-600 dark:text-gray-200">
-                Subject
-              </label>
+						<div className='mt-4'>
+							<label className='block mb-2 text-lg text-gray-600 dark:text-gray-200'>
+								Email address
+							</label>
+							{props.errors.email && props.touched.email ? (
+								<div className='text-red'>{props.errors.email}</div>
+							) : null}
+							<Field
+								name='email'
+								type='email'
+								placeholder='johndoe@example.com'
+								className='block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+							/>
+						</div>
+						<div className='mt-4'>
+							<label className='block mb-2 text-lg text-gray-600 dark:text-gray-200'>
+								Subject
+							</label>
 
-              <Field
-                onChange={(e: { target: { value: number } }) => {
-                  props.handleChange(e);
-                  const value = e.target.value;
-                  console.log(e.target.value);
-                  props.setFieldValue("message", emailTemplates[value].message);
-                }}
-                name="subject"
-                as="select"
-                placeholder="johndoe@example.com"
-                className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-              >
-                {emailTemplates.map((e, i) => (
-                  <option key={e.type} value={i}>
-                    {e.type}
-                  </option>
-                ))}
-              </Field>
-            </div>
+							<Field
+								onChange={(e: { target: { value: number } }) => {
+									props.handleChange(e);
+									const value = e.target.value;
+									props.setFieldValue('message', emailTemplates[value].message);
+								}}
+								name='subject'
+								as='select'
+								placeholder='johndoe@example.com'
+								className='block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'>
+								{emailTemplates.map((e, i) => (
+									<option key={e.type} value={i}>
+										{e.type}
+									</option>
+								))}
+							</Field>
+						</div>
 
-            <div className="w-full mt-4">
-              <label className="block mb-2 text-lg text-gray-600 dark:text-gray-200">
-                Message
-              </label>
-              {props.errors.message && props.touched.message ? (
-                <div className="text-red">{props.errors.message}</div>
-              ) : null}
-              <Field
-                name="message"
-                as="textarea"
-                className="block w-full h-32 px-5 py-2.5 mt-2  bg-white border rounded-lg md:h-56 focus:outline-none focus:ring focus:ring-opacity-40"
-                placeholder="Message"
-              />
-            </div>
-            <Button type="submit" className="w-full my-5 bg-primary text-white">
-              Send message
-            </Button>
-          </Formi>
-        )}
-      </Formik>
-    </div>
-  );
+						<div className='w-full mt-4'>
+							<label className='block mb-2 text-lg text-gray-600 dark:text-gray-200'>
+								Message
+							</label>
+							{props.errors.message && props.touched.message ? (
+								<div className='text-red'>{props.errors.message}</div>
+							) : null}
+							<Field
+								name='message'
+								as='textarea'
+								className='block w-full h-32 px-5 py-2.5 mt-2  bg-white border rounded-lg md:h-56 focus:outline-none focus:ring focus:ring-opacity-40'
+								placeholder='Message'
+							/>
+						</div>
+						<Button type='submit' className='w-full my-5 bg-primary text-white'>
+							Send message
+						</Button>
+					</Formi>
+				)}
+			</Formik>
+		</div>
+	);
 };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,128 +1,125 @@
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
-import { Email, Form } from "../components/Form";
-import { useQuery } from "../hooks/query";
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined';
+import { Email, Form } from '../components/Form';
+import { useQuery } from '../hooks/query';
 
 //Edit company details
 export const companyDetails = {
-  telephone: "+1800229933",
-  email: "hello@company.com",
-  address: "100 Smith Street Collingwood VIC 3066 AU",
-  messenger: "https://www.google.com",
+	telephone: '+14165798024',
+	email: 'hello@company.com',
+	address: '100 Smith Street Collingwood VIC 3066 AU',
+	messenger: 'https://www.google.com',
 };
 
 export const Contact = () => {
-  const query = useQuery();
-  const type = query.get("type") as Email["type"];
-  return (
-    <section className="bg-neutral relative  overflow-hidden">
-      <div className="blob absolute h-full w-full backdrop-blur-3xl z-10"></div>
+	const query = useQuery();
+	const type = query.get('type') as Email['type'];
+	return (
+		<section className='bg-neutral relative  overflow-hidden'>
+			<div className='blob absolute h-full w-full backdrop-blur-3xl z-10'></div>
 
-      <div className="blob  absolute left-1/2 top-1/2 [translate:-50%-60%] opacity-30 h-2/4 aspect-square rounded-full bg-gradient-to-r from-yellow to-primary animate-rotate "></div>
+			<div className='blob  absolute left-1/2 top-1/2 [translate:-50%-60%] opacity-30 h-2/4 aspect-square rounded-full bg-gradient-to-r from-yellow to-primary animate-rotate '></div>
 
-      <div className="navbar-spacer invisible pt-20 lg:py-20">
-        navbar plachoder
-      </div>
-      <div className="relative px-10 lg:px-32 z-20  w-full">
-        <div>
-          <p className="text-3xl lg:text-6xl z-20 font-bold dark:text-secondary">
-            Contact us
-          </p>
+			<div className='navbar-spacer invisible pt-20 lg:py-20'>
+				navbar plachoder
+			</div>
+			<div className='relative px-10 lg:px-32 z-20  w-full'>
+				<div>
+					<p className='text-3xl lg:text-6xl z-20 font-bold dark:text-secondary'>
+						Contact us
+					</p>
 
-          <h1 className="mt-2 text-xl z-20 ">Chat to our friendly team</h1>
+					<h1 className='mt-2 text-xl z-20 '>Chat to our friendly team</h1>
 
-          <p className="mt-3 text-lg z-20 ">
-            We’d love to hear from you. Please fill out this form or shoot us an
-            email.
-          </p>
-        </div>
+					<p className='mt-3 text-lg z-20 '>
+						We’d love to hear from you. Please fill out this form or shoot us an
+						email.
+					</p>
+				</div>
 
-        <div className="grid text-lg z-20 w-full justify-between pb-10 grid-cols-1 gap-12 mt-10 lg:grid-cols-2">
-          <div className="grid  grid-cols-1 gap-12 md:grid-cols-2">
-            <div>
-              <span className="inline-block p-3 text-primary rounded-full bg-white">
-                <EmailOutlinedIcon />
-              </span>
-              &nbsp;
-              <span className="mt-4 text-xl font-medium text-primary ">
-                Email
-              </span>
-              <p className="mt-2 text-lg ">
-                Our friendly team is here to help.
-              </p>
-              <p
-                className="mt-2 text-lg cursor-pointer text-red hover:text-primary hover:underline"
-                onClick={() => {
-                  window.open(`mailto:${companyDetails.email}`);
-                }}
-              >
-                {companyDetails.email}
-              </p>
-            </div>
+				<div className='grid text-lg z-20 w-full justify-between pb-10 grid-cols-1 gap-12 mt-10 lg:grid-cols-2'>
+					<div className='grid  grid-cols-1 gap-12 md:grid-cols-2'>
+						<div>
+							<span className='inline-block p-3 text-primary rounded-full bg-white'>
+								<EmailOutlinedIcon />
+							</span>
+							&nbsp;
+							<span className='mt-4 text-xl font-medium text-primary '>
+								Email
+							</span>
+							<p className='mt-2 text-lg '>
+								Our friendly team is here to help.
+							</p>
+							<p
+								className='mt-2 text-lg cursor-pointer text-red hover:text-primary hover:underline'
+								onClick={() => {
+									window.open(`mailto:${companyDetails.email}`);
+								}}>
+								{companyDetails.email}
+							</p>
+						</div>
 
-            <div>
-              <span className="inline-block p-3 text-primary rounded-full bg-white">
-                <ChatBubbleOutlineOutlinedIcon />
-              </span>
-              &nbsp;
-              <span className="mt-4 text-xl font-medium text-primary ">
-                Messenger chat
-              </span>
-              <p className="mt-2 text-lg ">
-                Our friendly team is here to help.
-              </p>
-              <a
-                className="mt-2 text-lg text-red hover:underline  hover:text-primary"
-                target="_blank"
-                href={companyDetails.messenger}
-              >
-                Start new chat
-              </a>
-            </div>
+						<div>
+							<span className='inline-block p-3 text-primary rounded-full bg-white'>
+								<ChatBubbleOutlineOutlinedIcon />
+							</span>
+							&nbsp;
+							<span className='mt-4 text-xl font-medium text-primary '>
+								Messenger chat
+							</span>
+							<p className='mt-2 text-lg '>
+								Our friendly team is here to help.
+							</p>
+							<a
+								className='mt-2 text-lg text-red hover:underline  hover:text-primary'
+								target='_blank'
+								href={companyDetails.messenger}>
+								Start new chat
+							</a>
+						</div>
 
-            <div>
-              <span className="inline-block p-3 text-primary rounded-full bg-white ">
-                <LocationOnOutlinedIcon />
-              </span>
-              &nbsp;
-              <span className="mt-4 text-xl font-medium text-primary">
-                Office
-              </span>
-              <p className="mt-2 text-lg text-gray-500 dark:text-gray-400">
-                Come say hello at our office HQ.
-              </p>
-              <p className="mt-2 text-lg text-blue-500 dark:text-blue-400  text-red hover:underline  hover:text-primary">
-                {companyDetails.address}
-              </p>
-            </div>
+						<div>
+							<span className='inline-block p-3 text-primary rounded-full bg-white '>
+								<LocationOnOutlinedIcon />
+							</span>
+							&nbsp;
+							<span className='mt-4 text-xl font-medium text-primary'>
+								Office
+							</span>
+							<p className='mt-2 text-lg text-gray-500 dark:text-gray-400'>
+								Come say hello at our office HQ.
+							</p>
+							<p className='mt-2 text-lg text-blue-500 dark:text-blue-400  text-red hover:underline  hover:text-primary'>
+								{companyDetails.address}
+							</p>
+						</div>
 
-            <div>
-              <span className="inline-block p-3 text-primary rounded-full bg-white ">
-                <PhoneOutlinedIcon />
-              </span>
-              &nbsp;
-              <span className="mt-4 text-xl font-medium text-primary ">
-                Phone
-              </span>
-              <p className="mt-2 text-lg text-gray-500 dark:text-gray-400">
-                Mon-Fri from 8am to 5pm.
-              </p>
-              <p
-                onClick={() => {
-                  window.open(`tel:${companyDetails.telephone}`);
-                }}
-                className="mt-2 hover:text-primary cursor-pointer text-lg text-blue-500 dark:text-blue-400  text-red hover:underline  "
-              >
-                {companyDetails.telephone}
-              </p>
-            </div>
-          </div>
+						<div>
+							<span className='inline-block p-3 text-primary rounded-full bg-white '>
+								<PhoneOutlinedIcon />
+							</span>
+							&nbsp;
+							<span className='mt-4 text-xl font-medium text-primary '>
+								Phone
+							</span>
+							<p className='mt-2 text-lg text-gray-500 dark:text-gray-400'>
+								Mon-Fri from 8am to 5pm.
+							</p>
+							<p
+								onClick={() => {
+									window.open(`tel:${companyDetails.telephone}`);
+								}}
+								className='mt-2 hover:text-primary cursor-pointer text-lg text-blue-500 dark:text-blue-400  text-red hover:underline  '>
+								{companyDetails.telephone}
+							</p>
+						</div>
+					</div>
 
-          <Form type={type} />
-        </div>
-      </div>
-    </section>
-  );
+					<Form type={type} />
+				</div>
+			</div>
+		</section>
+	);
 };
